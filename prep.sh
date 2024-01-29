814e25c01c5fb5e50689fffd2fc5b58400a694a1
Matt Garber <matthew.garber@childrens.harvard.edu>
2024-01-29 11:14:15 -0500

    Adjust ordering

--- prep.sh
+++ prep.sh
@@ -58,5 +58,5 @@ git submodule update --init --remote
 # Only use nav_order 10-20 for submodules
 copy_docs cumulus-etl etl 10
 copy_docs cumulus-library library 11
-copy_docs chart-review chart-review 12
-copy_docs cumulus-aggregator aggregator 13
+copy_docs cumulus-aggregator aggregator 12
+copy_docs chart-review chart-review 13
